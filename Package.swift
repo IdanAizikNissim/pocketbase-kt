45748ecce359b9982850fe1e7942134c1149c791
runner <runner@Mac-1729803322503.local>
2024-10-24 21:49:23 +0000

    KMM SPM package release for 0.1.3

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+// BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
+let remoteKotlinUrl = "https://maven.pkg.github.com/IdanAizikNissim/pocketbase-kt/io/pocketbase/shared-kmmbridge/0.1.3/shared-kmmbridge-0.1.3.zip"
+let remoteKotlinChecksum = "3a2ab890296ef62f26e28dee3f5ff948ca93a4b206d495ae4d6b38edb74e33b7"
+let packageName = "PocketBase"
+// END KMMBRIDGE BLOCK
+
+let package = Package(
+    name: packageName,
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: packageName,
+            targets: [packageName]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: packageName,
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
+        )
+        ,
+    ]
+)
