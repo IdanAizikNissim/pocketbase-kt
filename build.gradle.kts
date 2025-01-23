0cc9bacee457df2621b88e699ee97c931acf1e30
idan <idan.aizik.nissim@gmail.com>
2025-01-23 21:06:22 +0200

    Release 0.1.12

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,14 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach 
     }
 }
 
+subprojects {
+    val GROUP: String by project
+    val LIBRARY_VERSION: String by project
+
+    group = GROUP
+    version = LIBRARY_VERSION
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
