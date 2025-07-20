a3d1351108938ab783399738fb657628c8c8abe1
Idan Aizik Nissim <idan@spikelabsinc.com>
2025-07-20 11:12:24 +0300

    list pb project tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.androidLibrary).apply(false)
     alias(libs.plugins.kotlinMultiplatform).apply(false)
     alias(libs.plugins.serialization).apply(false)
+    alias(libs.plugins.ktlint).apply(false)
     alias(libs.plugins.kmmbridge).apply(false)
     alias(libs.plugins.skie).apply(false)
 }
