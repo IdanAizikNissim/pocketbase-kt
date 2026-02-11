449f3bf440df206443377ee07e85abcacf227f29
idan <idan.aizik.nissim@gmail.com>
2026-02-11 22:29:31 +0200

    Revert "Fix import statement from ComposeApp to SampleApp"
    This reverts commit a5b1b67c96cb3e8c70f5b6d19a0e8dcd3d16b2a4.

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,6 +1,6 @@
 import UIKit
 import SwiftUI
-import SampleApp
+import ComposeApp
 
 struct ComposeView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
